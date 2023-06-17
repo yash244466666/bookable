ca845eab851befce78a0b9073ae6c703f7133d4e
Yash <yash.solo.000@gmail.com>
2023-06-17 22:04:40 +0600

    Practics rails api  and react front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Books from './components/books';
+
+const API_URL = "http://127.0.0.1:3000/api/v1/books";
+
+function getApiData() {
+  return axios.get(API_URL).then(response => response.data);
+}
 
 function App() {
+
+  const [books, setBoooks] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getApiData().then((items) => {
+      if (mounted) {
+        setBoooks(items);
+      }
+    });
+    return () => (mounted = false);
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My React App</h1>
+      <Books books={books} />
+
     </div>
   );
 }
